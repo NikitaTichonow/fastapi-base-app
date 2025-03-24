faa74f0d7fb914dfb11123e79950464aba643965
Nikita <niktos@icloud.com>
2025-03-24 22:41:24 +0300

    create main settings

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from api import router as api_router
+from core.config import settings
 
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api.prefix)
+
 
