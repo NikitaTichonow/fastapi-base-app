779c1b00d644292a9452f58c00a32ab3c585fe99
Nikita <niktos@icloud.com>
2025-03-25 23:35:26 +0300

    add db helper dispose

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    print("dispouse engine")
+    await db_helper.dispouse()
+
+
+app = FastAPI(
+    lifespan = lifespan,
+)
 app.include_router(api_router, prefix=settings.api.prefix)
+
 
 
