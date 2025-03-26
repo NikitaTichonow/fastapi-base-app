4639a5fcead73d9d7795469bdec06adb50d3caa0
Nikita <niktos@icloud.com>
2025-03-27 00:09:53 +0300

    add naming convection or models

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -7,8 +7,10 @@ class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+
 
 class DataBaseConfig(BaseModel):
     url: PostgresDsn
@@ -16,6 +18,14 @@ class DataBaseConfig(BaseModel):
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
+
+    naming_convection: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
 
 
 class Settings(BaseSettings):
@@ -27,7 +37,8 @@ class Settings(BaseSettings):
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    db: DataBaseConfig 
+    db: DataBaseConfig
+
 
 settings = Settings()
 print(settings.db.url)
